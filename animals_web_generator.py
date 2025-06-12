3b45fccbe9cbc0c2d51d24f79d02cd3e86b303f5
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-06-12 23:11:04 +0200

    Let the user choose an animal. Delete unused files for filtering an animal by its skin type.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,16 +1,15 @@
-from manage_skin_type import *
 from manage_html import *
 from html_communication import search_for_animal_in_api
 
 
-def get_user_input(unique_skin_types):
+def get_user_input_for_skin_type(unique_skin_types):
     """
     Gets the selected skin type from the user input.
     :param unique_skin_types: Unique skin types for all animals.
     :return: Selected skin type by the user.
     """
     while True:
-        selected_skin_type = input("Select a skin type to use: ")
+        selected_skin_type = input("Select a skin type to use: ").strip()
         if selected_skin_type in unique_skin_types:
             break
         print("Invalid skin type.")
@@ -25,29 +24,23 @@ def main():
     all the animals with the selected skin type.
     """
     try:
-        # get JSON data from API
-        animals_data = search_for_animal_in_api("fox")
+        chosen_animal = input("Enter a name of an animal: ").strip()
 
-        # manage the skin type
-        unique_skin_types = get_unique_skin_types(animals_data)
-        print_skin_types(unique_skin_types)
-        selected_skin_type = get_user_input(unique_skin_types)
-        animals_with_selected_skin, counter_animals_without_skin_type = (
-            collect_animals_with_selected_skin_type(
-                selected_skin_type, animals_data))
-        print_animals_without_skin_type(counter_animals_without_skin_type)
+        # get JSON data from API
+        animals_data = search_for_animal_in_api(chosen_animal)
+
+        if not animals_data:
+            print("No animals found.")
+            return
 
         # manage the HTML
-        html_formatted_animals = serialize_all_animals(
-            animals_with_selected_skin)
+        html_formatted_animals = serialize_all_animals(animals_data)
         template_html_for_animals = get_template_html()
         full_html_with_animals = insert_new_data_in_html_template(
             template_html_for_animals, html_formatted_animals)
         file_name = "animals.html"
         create_html_file(file_name, full_html_with_animals)
-        print(f"The website {file_name} was generated for "
-              f"animals with selected skin type.")
-
+        print(f"Website was successfully generated to the file {file_name}.")
     except Exception as error:
         print(f"Unexpected error: {error}")
 
